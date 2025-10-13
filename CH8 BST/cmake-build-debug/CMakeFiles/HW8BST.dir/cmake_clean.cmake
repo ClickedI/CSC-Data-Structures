62c58978679cab8b5d810464839f1dbc10b6cc1d
Erick <erickropi@yahoo.com>
2025-10-13 15:11:35 -0400

    Completed HW7

---
+++ CH8 BST/cmake-build-debug/CMakeFiles/HW8BST.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW8BST.dir/C_/Users/eropi/Documents/Personal/CSC-Data-Structures/CH7_HW_Rivers/Rivers.cpp.obj"
+  "CMakeFiles/HW8BST.dir/C_/Users/eropi/Documents/Personal/CSC-Data-Structures/CH7_HW_Rivers/Rivers.cpp.obj.d"
+  "CMakeFiles/HW8BST.dir/Charge.cpp.obj"
+  "CMakeFiles/HW8BST.dir/Charge.cpp.obj.d"
+  "CMakeFiles/HW8BST.dir/Contact.cpp.obj"
+  "CMakeFiles/HW8BST.dir/Contact.cpp.obj.d"
+  "CMakeFiles/HW8BST.dir/Customer.cpp.obj"
+  "CMakeFiles/HW8BST.dir/Customer.cpp.obj.d"
+  "CMakeFiles/HW8BST.dir/ListOfCharge.cpp.obj"
+  "CMakeFiles/HW8BST.dir/ListOfCharge.cpp.obj.d"
+  "CMakeFiles/HW8BST.dir/main.cpp.obj"
+  "CMakeFiles/HW8BST.dir/main.cpp.obj.d"
+  "HW8BST.exe"
+  "HW8BST.exe.manifest"
+  "HW8BST.pdb"
+  "libHW8BST.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW8BST.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CH8 BST/cmake-build-debug/CMakeFiles/unittest.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/eropi/Documents/Personal/CSC-Data-Structures/CH7 HW Rivers/Rivers.cpp" "CMakeFiles/unittest.dir/C_/Users/eropi/Documents/Personal/CSC-Data-Structures/CH7_HW_Rivers/Rivers.cpp.obj" "gcc" "CMakeFiles/unittest.dir/C_/Users/eropi/Documents/Personal/CSC-Data-Structures/CH7_HW_Rivers/Rivers.cpp.obj.d"
+  "C:/Users/eropi/Documents/Personal/CSC-Data-Structures/CH8 BST/Charge.cpp" "CMakeFiles/unittest.dir/Charge.cpp.obj" "gcc" "CMakeFiles/unittest.dir/Charge.cpp.obj.d"
+  "C:/Users/eropi/Documents/Personal/CSC-Data-Structures/CH8 BST/Contact.cpp" "CMakeFiles/unittest.dir/Contact.cpp.obj" "gcc" "CMakeFiles/unittest.dir/Contact.cpp.obj.d"
+  "C:/Users/eropi/Documents/Personal/CSC-Data-Structures/CH8 BST/Customer.cpp" "CMakeFiles/unittest.dir/Customer.cpp.obj" "gcc" "CMakeFiles/unittest.dir/Customer.cpp.obj.d"
+  "C:/Users/eropi/Documents/Personal/CSC-Data-Structures/CH8 BST/ListOfCharge.cpp" "CMakeFiles/unittest.dir/ListOfCharge.cpp.obj" "gcc" "CMakeFiles/unittest.dir/ListOfCharge.cpp.obj.d"
+  "C:/Users/eropi/Documents/Personal/CSC-Data-Structures/CH8 BST/cmake-build-debug/runner.cpp" "CMakeFiles/unittest.dir/runner.cpp.obj" "gcc" "CMakeFiles/unittest.dir/runner.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
